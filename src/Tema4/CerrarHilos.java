32437a6b4f70717d2e8071494833ea1f73f14619
Iván Pérez Huete <ivan120695@gmail.com>
2019-02-19 16:36:26 +0100

    Ejercicio 4_1

---
+++ src/Tema4/CerrarHilos.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package Tema4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Ivan.Perez
+ *
+ *  https://github.com/IvanPerez9
+ */
+public class CerrarHilos {
+
+	public static void generateNumbers() {
+		try {
+			PrintWriter writer = new PrintWriter("output.txt");
+			while (true) {
+				// numero primo aleatorio
+				BigInteger prime = BigInteger.probablePrime(1024,new Random());
+				writer.println(prime.toString());
+				// Si aqui le pongo un sleep tengo que controlar que no me lo bloqueen mientras duerme
+				if(Thread.interrupted()){ // Aqui miro si me interrumpen
+					writer.append("Fin fichero");
+					writer.close();
+					return;
+					// Return vacio para que termine de ejecutar el codigo aqui
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Exception using file");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	/*
+	 * Metodo interrupted, si me han interrumpido libero los recursos.
+	 * Señal que le mando.
+	 * 
+	 * El hilo que es interrumpido es el que decide que hace cuando le interrumpen
+	 */
+	public static void main(String[] args) {
+		Thread t = new Thread(() -> {
+			generateNumbers();
+		});
+		t.start();
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Pulse ENTER para finalizar...");
+		teclado.nextLine();
+		t.interrupt();
+		System.out.println("Hilo interrumpido.");
+		teclado.close();
+	}
+}
---
+++ src/Tema4/Ejercicio4_1.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package Tema4;
+
+/**
+ * @author Ivan.Perez
+ *
+ *  https://github.com/IvanPerez9
+ */
+public class Ejercicio4_1 {
+
+	/*
+	 * Similar code to Messages in class 
+	 */
+	
+	public static void hiloSecundario() {
+		boolean continuar = true;
+		while(continuar) {
+			System.out.println("MSJ: La vida es bella");
+			System.out.println("MSJ: O no...");
+			System.out.println("MSJ: Y molestan");
+			if (Thread.interrupted()){
+				System.out.println("MSJ: Se acabo");
+				continuar = false;
+				return;
+			}
+		}
+	}
+	
+	// Borrar las e.printStackTrace() si no hace falta
+	
+	public static void main(String[] args) {
+		Thread hiloMensajes = new Thread(() -> {
+			hiloSecundario();
+		});
+		
+		hiloMensajes.start();
+		
+		// No puedo hacer join de golpe porque los bloqueo. Y no quiero bloquear porque se llama uno al otro
+		
+		boolean continuar = true;
+		int contador = 0;
+		while (continuar) {
+			if (!hiloMensajes.isAlive()) {
+				continuar = false;
+			} else {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (contador == 5) {
+					System.out.println("Cansado de esperar");
+					hiloMensajes.interrupt();
+					// Hago el join para esperar que termine y no interrumpirle a la mitad 
+					try {
+						hiloMensajes.join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					continuar = false;
+				} else {
+					System.out.println("Todavia esperando");
+					contador++;
+				}
+			}		}
+		System.out.println("Por fin");
+	}
+	
+}
