1785d3fadd420c221a0a3ef3d1443cb6a13a40e5
Iván Pérez Huete <ivan120695@gmail.com>
2019-03-10 10:48:21 +0100

    ejercicio CountDownLatch Repaso

--- src/EjerciciosRepaso/Ejercicio4_7_Repaso.java
+++ src/EjerciciosRepaso/Ejercicio4_7_Repaso.java
@@ -3,6 +3,8 @@
  */
 package EjerciciosRepaso;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author Ivan.Perez
  *
@@ -11,7 +13,69 @@ package EjerciciosRepaso;
 public class Ejercicio4_7_Repaso {
 
 	/*
+	 * Ejercicio de CountDownLatch , diagrama de precedencia 
 	 * 
+	 * Los hilos tiene que esperar a que el contador este a 0 para continuar 
+	 * Se bloquean con await y se deja seguir con countDown -> Tantas veces como se especifique en el constructor del objeto
 	 */
+	
+	static CountDownLatch latchD = new CountDownLatch(2);
+	static CountDownLatch latchBG = new CountDownLatch(1);
+	static CountDownLatch latchC = new CountDownLatch(1);
+	static CountDownLatch latchE = new CountDownLatch(1);
+	static CountDownLatch latchH = new CountDownLatch(1);
+	
+	
+	public static void p1 () {
+		try {
+			System.out.println("A");
+			latchD.countDown();
+			latchBG.await();
+			System.out.println("B");
+			latchC.await();
+			latchE.countDown();
+			latchH.countDown();
+			System.out.println("C");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void p2 () {
+		try {
+			latchD.await();
+			System.out.println("D");
+			latchBG.countDown();
+			latchE.await(); // el E aqui se espera a que B,G y H se ejecuten
+			System.out.println("E");
+			latchC.countDown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void p3 () {
+		try {
+			System.out.println("F");
+			latchD.countDown();
+			latchBG.await();
+			System.out.println("G");
+			latchH.countDown();
+			System.out.println("H");
+			latchE.countDown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Thread p1 = new Thread (() -> p1() );
+		Thread p2 = new Thread (() -> p2() );
+		Thread p3 = new Thread (() -> p3() );
+		
+		p1.start();
+		p2.start();
+		p3.start();
+	}
 	
 }
