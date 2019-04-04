38c380112469b169d9789a4f7aea07e28843b246
Iván Pérez Huete <ivan120695@gmail.com>
2019-04-04 18:19:05 +0200

    ignore

--- src/HojaEjercicios3/EjerciciosPersonajes.java
+++ src/HojaEjercicios3/EjerciciosPersonajes.java
@@ -7,12 +7,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 /**
  * @author Ivan.Perez
@@ -29,38 +26,32 @@ public class EjerciciosPersonajes {
 	 * 
 	 */
 	public EjerciciosPersonajes(String path) throws FileNotFoundException, IOException {
-		personajes = new ArrayList<>();
-		
-		try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
-			personajes = bf.lines()
-					.skip(1)
-					.map(line -> line.trim().split(";"))
-					.filter(line -> line.length == 12)
-					.map((col -> {
-						Personajes personaje = null;
-						try {
-							personaje = new Personajes(
-									col[0],
-									col[1],
-									Integer.parseInt(col[2]),
-									col[3],
-									col[4],
-									col[5],
-									col[6],
-									col[7],
-									col[8],
-									Integer.parseInt(col[9]),
-									Integer.parseInt(col[10]),
-									Integer.parseInt(col[11]),
-									Integer.parseInt(col[12]));
-						} catch (ParseException e) {
-							return e;
-						}
-						return personaje;
-					})
-					.collect(Collectors.toList()));
-		}
-	}
+        personajes = new ArrayList<>();
+       
+        try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
+            personajes = bf.lines()
+                    .skip(1)
+                    .map(line -> line.trim().split(";"))
+                    .filter(line -> line.length == 12)
+                    .map(col ->
+                        new Personajes(
+                                    col[0],
+                                    col[1],
+                                    Integer.parseInt(col[2]),
+                                    col[3],
+                                    col[4],
+                                    col[5],
+                                    col[6],
+                                    col[7],
+                                    col[8],
+                                    Integer.parseInt(col[9]),
+                                    Integer.parseInt(col[10]),
+                                    Integer.parseInt(col[11]),
+                                    Integer.parseInt(col[12]))
+                    )
+                    .collect(Collectors.toList());
+        }
+    }
 	
 	
 	
